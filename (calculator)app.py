9b8b69a9f5483b68bdf0623e9ad42e0053e8271d
Ranjith GS <119926692+Ranjith123407@users.noreply.github.com>
2023-11-18 20:29:21 +0530

    Add files via upload

---
+++ (calculator)app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']
+
+    result = perform_operation(num1, num2, operation)
+    return render_template('index.html', result=result, num1=num1, num2=num2, operation=operation)
+
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return 'Cannot divide by zero'
+
+if __name__ == '__main__':
+    app.run(debug=True)
